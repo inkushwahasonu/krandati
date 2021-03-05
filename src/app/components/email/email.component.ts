bda18951d1af6202c9312b435cf5c34aa1634ed3
inkushwahasonu <inkushwahasonu@gmail.com>
2021-03-05 20:04:36 +0530

    first angular deployed

---
+++ src/app/components/email/email.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email',
+  templateUrl: './email.component.html',
+  styleUrls: ['./email.component.css']
+})
+export class EmailComponent implements OnInit {
+
+  data = {
+    to:"",
+    subject:"",
+    message:"",
+  }
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  doSubmitForm(){
+    console.log('try to submit form');
+  }
+
+}
